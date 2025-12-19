5caa751b00e9d8e0e2d86f0ef8712dd56544a2e7
Sabin Shahi <ss.sabin.shahi@gmail.com>
2025-12-20 05:41:48 +0545

    Added CLI support using argparse

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 from src.dataset_search import search_dataset
 from src.downloader import download_dataset
 from src.analysis import load_dataset, process_dataset
@@ -5,28 +6,53 @@ from src.visualization import get_visualizations
 from src.report_generator import fetch_report
 from src.logger import initialize_logger
 
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Automated Kaggle Dataset Fetching and Analysis Pipeline"
+    )
+
+    parser.add_argument(
+        "--query",
+        required=True,
+        help="Keyword to search on Kaggle for dataset"
+    )
+
+    parser.add_argument(
+        "--report",
+        default="html",
+        choices=["html"],
+        help="Report Format (default: html)"
+    )
+
+    return parser.parse_args()
+
 def main():
+    args = parse_args()
     logger = initialize_logger()
+
     logger.info("Automation Started")
+    logger.info(f"CLI arguments: query={args.query}, report={args.report}")
 
-    query = input("Enter dataset keyword: ")
-    logger.info(f"Dataset query received: {query}")
+    try:
+        dataset_ref = search_dataset(args.query)
+        logger.info(f"Found dataset: {dataset_ref}")
 
-    dataset_ref = search_dataset(query)
-    logger.info(f'Found dataset: {dataset_ref}')
+        download_dataset(dataset_ref)
+        logger.info("Dataset downloaded successfully")
 
-    download_dataset(dataset_ref)
-    logger.info('Dataset downloaded successfully')
+        df = load_dataset()
+        logger.info("Dataset loaded")
 
-    df = load_dataset()
-    process_dataset(df)
+        process_dataset(df)
+        logger.info("Data Processing Completed.")
 
-    get_visualizations(df)
-    logger.info("Visualizations files created")
+        get_visualizations(df)
+        logger.info("Visualizations files created")
 
-    report_path = fetch_report(df)
-    logger.info(f'Report generated at: {report_path}')
-
+        report_path = fetch_report(df)
+        logger.info(f'Report generated at: {report_path}')
+    except Exception as e:
+        logger.exception(f'Pipeline execution failed {e}')
 
 if __name__=="__main__":
     main()
